daa11c80925ac8d0a304dd151fde848ec01d2acc
Caio Christhian <caiochristhianz@gmail.com>
2022-07-07 15:39:31 -0300

    criando a página Home e refatorando Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,91 @@
+import {
+  Container, Header, ListContainer, Card,
+  InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Home';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar Contato" />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Kuro</strong>
+              <small>Instagram</small>
+            </div>
+            <span>caiochristhianz@gmail.com</span>
+            <span>(63) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Kuro</strong>
+              <small>Instagram</small>
+            </div>
+            <span>caiochristhianz@gmail.com</span>
+            <span>(63) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Kuro</strong>
+              <small>Instagram</small>
+            </div>
+            <span>caiochristhianz@gmail.com</span>
+            <span>(63) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
