b6556272c2fd494171310323d27d1dbc32e995ab
Caio Christhian <caiochristhianz@gmail.com>
2022-07-18 14:32:47 -0300

    criando o componente button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -10,7 +11,15 @@ export default function NewContact() {
       <Input placeholder="Nome" />
       <Select>
         <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
       </Select>
+      <Button type="button">
+        Cadastrar
+      </Button>
+      <Button disabled type="button">
+        Cadastrar
+      </Button>
     </>
   );
 }
