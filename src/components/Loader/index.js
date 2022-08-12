9c645339c6382703fcd1bdda64981365c59902a6
Caio Christhian <caiochristhianz@gmail.com>
2022-08-11 22:12:34 -0300

    melhoria de UX com status de loading

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,7 +1,13 @@
 import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+
 import { Overlay } from './styles';
 
-export default function Loader() {
+export default function Loader({ isLoading }) {
+  if (!isLoading) {
+    return null;
+  }
+
   return createPortal(
     <Overlay>
       <div className="loader">
@@ -11,3 +17,7 @@ export default function Loader() {
     document.getElementById('loader-root'),
   );
 }
+
+Loader.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,10 +10,14 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
+import Loader from '../../components/Loader';
+import delay from '../../utils/delay';
+
 export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const filteredContacts = useMemo(() => (
     contacts.filter((contact) => (
@@ -22,13 +26,20 @@ export default function Home() {
   ), [contacts, searchTerm]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
+        await delay(500);
+
         const json = await response.json();
         setContacts(json);
       })
       .catch((error) => {
         console.log('erro', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [orderBy]);
 
@@ -44,6 +55,8 @@ export default function Home() {
 
   return (
     <Container>
+      <Loader isLoading={isLoading} />
+
       <InputSearchContainer>
         <input
           value={searchTerm}
