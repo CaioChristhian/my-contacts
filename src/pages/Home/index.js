a5652ab2c9b57431c71b9f18d7c775d3376733ef
Caio Christhian <caiochristhianz@gmail.com>
2022-08-25 11:14:54 -0300

    resolvendo problemas de loop infinito no useEffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 
-import { useEffect, useState, useMemo } from 'react';
+import {
+  useEffect, useState, useMemo, useCallback,
+} from 'react';
 import {
   Container, Header, ListHeader, Card,
   InputSearchContainer, ErrorContainer,
@@ -29,7 +31,7 @@ export default function Home() {
     ))
   ), [contacts, searchTerm]);
 
-  async function loadContacts() {
+  const loadContacts = useCallback(async () => {
     try {
       setIsLoading(true);
 
@@ -42,11 +44,11 @@ export default function Home() {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [orderBy]);
 
   useEffect(() => {
     loadContacts();
-  }, [orderBy]);
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
     setOrderBy(
