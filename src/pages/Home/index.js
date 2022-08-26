231bc3ed008e854683733a5d8855d1e7b523d830
Caio Christhian <caiochristhianz@gmail.com>
2022-08-26 12:52:41 -0300

    exibindo mensagem de nenhum contato encontrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ import {
 import {
   Container, Header, ListHeader, Card,
   InputSearchContainer, ErrorContainer,
-  EmptyListContainer,
+  EmptyListContainer, SearchNotFoundContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -16,6 +16,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Button from '../../components/Button';
 import Loader from '../../components/Loader';
@@ -40,7 +41,6 @@ export default function Home() {
       setIsLoading(true);
 
       const contactsList = await ContactsService.listContacts(orderBy);
-      // const contactsList = []; await ContactsService.listContacts(orderBy);
 
       setHasError(false);
       setContacts(contactsList);
@@ -131,6 +131,16 @@ export default function Home() {
                 para cadastrar o seu primeiro!
               </p>
             </EmptyListContainer>
+          )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+
+              <span>
+                Nenhum resultado foi encontrado para <strong>{searchTerm}</strong>.
+              </span>
+            </SearchNotFoundContainer>
           )}
 
           {filteredContacts.length > 0 && (
